4839810ca39cc76dd64434b9db5fca2f48709879
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2024-05-26 17:25:40 -0300

    feat: add app_title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { env } from './infra/env';
+
 function App() {
-  return <h1>repbox</h1>;
+  return (
+    <>
+      <h1>repbox</h1>
+
+      <h2>APP_TITLE: {env.VITE_APP_TITLE}</h2>
+    </>
+  );
 }
 
 export default App;
