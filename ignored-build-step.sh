ee43f85b959aa7b0451e0491f17c9fee9dd200fd
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2024-05-26 19:33:59 -0300

    chore: change ignored build step script

--- ignored-build-step.sh
+++ ignored-build-step.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VITE_VERCEL_GIT_COMMIT_REF: $VITE_VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VITE_VERCEL_GIT_COMMIT_REF" == "staging" || "$VITE_VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
